37845022573602b857170bbaebd79d3c2ba8eb54
Matheus <matheusdeoliveiraes@gmail.com>
2024-04-17 22:24:05 -0300

    Tendo um erro com BEGIN no LAB_02

---
+++ LAB_02/Lab/parser.h
@@ -0,0 +1,106 @@
+/* A Bison parser, made by GNU Bison 3.8.2.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
+   Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
+
+#ifndef YY_YY_PARSER_H_INCLUDED
+# define YY_YY_PARSER_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token kinds.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    PROGRAM = 258,                 /* PROGRAM  */
+    ID = 259,                      /* ID  */
+    SEMI = 260,                    /* SEMI  */
+    VAR = 261,                     /* VAR  */
+    BOOL = 262,                    /* BOOL  */
+    INT = 263,                     /* INT  */
+    REAL = 264,                    /* REAL  */
+    STRING = 265,                  /* STRING  */
+    BEGIN = 266,                   /* BEGIN  */
+    END = 267,                     /* END  */
+    IF = 268,                      /* IF  */
+    THEN = 269,                    /* THEN  */
+    ELSE = 270,                    /* ELSE  */
+    REPEAT = 271,                  /* REPEAT  */
+    UNTIL = 272,                   /* UNTIL  */
+    ASSIGN = 273,                  /* ASSIGN  */
+    READ = 274,                    /* READ  */
+    WRITE = 275,                   /* WRITE  */
+    LT = 276,                      /* LT  */
+    EQ = 277,                      /* EQ  */
+    PLUS = 278,                    /* PLUS  */
+    MINUS = 279,                   /* MINUS  */
+    TIMES = 280,                   /* TIMES  */
+    OVER = 281,                    /* OVER  */
+    LPAR = 282,                    /* LPAR  */
+    RPAR = 283,                    /* RPAR  */
+    TRUE = 284,                    /* TRUE  */
+    FALSE = 285,                   /* FALSE  */
+    INT_VAL = 286,                 /* INT_VAL  */
+    REAL_VAL = 287,                /* REAL_VAL  */
+    STR_VAL = 288                  /* STR_VAL  */
+  };
+  typedef enum yytokentype yytoken_kind_t;
+#endif
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+
+int yyparse (void);
+
+
+#endif /* !YY_YY_PARSER_H_INCLUDED  */
---
+++ LAB_02/Lab/parser.y
@@ -0,0 +1,116 @@
+%output "parser.c"
+%defines "parser.h"
+%define parse.error verbose
+%define parse.lac full
+
+%{
+#include <stdio.h>
+#include <string.h>
+int yylex(void);
+void yyerror(const char *s) {
+    fprintf(stderr, "%s\n", s);
+}
+%}
+
+%token PROGRAM ID SEMI VAR BOOL INT REAL STRING
+%token BEGIN END IF THEN ELSE REPEAT UNTIL ASSIGN READ WRITE LT EQ PLUS MINUS TIMES OVER LPAR RPAR TRUE FALSE INT_VAL REAL_VAL STR_VAL
+
+//%left PLUS MINUS
+//%left TIMES OVER
+//%right ASSIGN
+
+%%
+
+program:
+    PROGRAM ID SEMI vars_sect stmt_sect
+    ;
+
+vars_sect:
+    VAR opt_var_decl
+    ;
+
+opt_var_decl:
+    var_decl_list
+    | /* vazio */
+    ;
+
+var_decl_list:
+    var_decl_list var_decl
+    | var_decl
+    ;
+
+var_decl:
+    type_spec ID SEMI
+    ;
+
+type_spec:
+    BOOL
+    | INT
+    | REAL
+    | STRING
+    ;
+
+stmt_sect:
+    BEGIN stmt_list END
+    ;
+
+stmt_list:
+    stmt_list stmt
+    | stmt
+    ;
+
+stmt:
+    if_stmt
+    | repeat_stmt
+    | assign_stmt
+    | read_stmt
+    | write_stmt
+    ;
+
+if_stmt:
+    IF expr THEN stmt_list END
+    | IF expr THEN stmt_list ELSE stmt_list END
+    ;
+
+repeat_stmt:
+    REPEAT stmt_list UNTIL expr
+    ;
+
+assign_stmt:
+    ID ASSIGN expr SEMI
+    ;
+
+read_stmt:
+    READ ID SEMI
+    ;
+
+write_stmt:
+    WRITE expr SEMI
+    ;
+
+expr:
+    expr LT expr
+    | expr EQ expr
+    | expr PLUS expr
+    | expr MINUS expr
+    | expr TIMES expr
+    | expr OVER expr
+    | LPAR expr RPAR
+    | TRUE
+    | FALSE
+    | INT_VAL
+    | REAL_VAL
+    | STR_VAL
+    | ID
+    ;
+
+%%
+
+int main(void) {
+    if (yyparse() == 0) {
+        printf("PARSE SUCCESSFUL!\n");
+    } else {
+        printf("PARSE FAILED!\n");
+    }
+    return 0;
+}
